6d95a4cb0d22c365a6f0a0233c0ea23b98297ab4
Roy Gotlieb <rgotlieb@qumulo.com>
2022-07-18 11:01:45 -0700

    added boilerplate

---
+++ qumulo/provider.go
@@ -0,0 +1,76 @@
+package qumulo
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Provider -
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"host": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("QUMULO_HOST", nil),
+			},
+			"port": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("QUMULO_PORT", nil),
+			},
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("QUMULO_USERNAME", nil),
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("QUMULO_PASSWORD", nil),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"qumulo_user":  resourceQumuloUser(),
+			"qumulo_group": resourceQumuloGroup(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			/* "qumulo_version":             dataSourceQumuloVersion(),
+			"qumulo_nfs_exports":         dataSourceQumuloNFSExports(),
+			"qumulo_network_connections": dataSourceQumuloNetworkConnections(), */
+			"qumulo_user":  dataSourceQumuloUser(),
+			"qumulo_group": dataSourceQumuloGroup(),
+		},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+
+	host := d.Get("host").(string)
+	port := d.Get("port").(string)
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	if (username != "") && (password != "") {
+		c, err := NewClient(&host, &port, &username, &password)
+		if err != nil {
+			return nil, diag.FromErr(err)
+		}
+
+		return c, diags
+	}
+
+	c, err := NewClient(nil, nil, nil, nil)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+
+	return c, diags
+}
