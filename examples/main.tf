38352f3fb007b5675ed3397bce906c98cc798a9c
Roy Gotlieb <rgotlieb@qumulo.com>
2022-07-21 10:37:30 -0700

    updated simnode connection

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,5 @@
+
+
 terraform {
   required_providers {
     qumulo = {
@@ -10,13 +12,13 @@ terraform {
 provider "qumulo" {
   username = "admin"
   password = "Admin123"
-  host= "10.116.100.110"
-  port= "17437"
+  host= "10.116.10.215"
+  port= "17728"
 }
 
 variable "some_cluster_name" {
   type    = string
-  default = "NewName"
+  default = "InigoMontoya"
 }
 
 variable "some_cert" {
