6522db7de4b6da7c9d0f99446d61eb908d25bd7f
amanning32 <ajmanning@tampabay.rr.com>
2022-07-27 15:53:20 -0700

    Add timeouts to NFS exports

--- qumulo/resource_nfs_export.go
+++ qumulo/resource_nfs_export.go
@@ -3,6 +3,7 @@ package qumulo
 import (
 	"context"
 	"strconv"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -39,6 +40,13 @@ func resourceNfsExport() *schema.Resource {
 		ReadContext:   resourceNfsExportRead,
 		UpdateContext: resourceNfsExportUpdate,
 		DeleteContext: resourceNfsExportDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(1 * time.Minute),
+			Update: schema.DefaultTimeout(1 * time.Minute),
+			Delete: schema.DefaultTimeout(1 * time.Minute),
+		},
+
 		Schema: map[string]*schema.Schema{
 			"id": &schema.Schema{
 				Type:     schema.TypeString,
