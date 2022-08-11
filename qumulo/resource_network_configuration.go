a43020d2567485904d34c994669bfa3c307223d5
Prajwala TM <prajwala.tm@gmail.com>
2022-08-11 14:15:43 -0700

    Fix review comments

--- qumulo/resource_network_configuration.go
+++ qumulo/resource_network_configuration.go
@@ -127,7 +127,7 @@ func resourceNetworkConfiguration() *schema.Resource {
 			State: func(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 				// d.Id() here is the last argument passed to the
 				//`terraform import RESOURCE_TYPE.RESOURCE_NAME RESOURCE_ID` command
-				networkId, interfaceId, err := parseImportID(d.Id())
+				interfaceId, networkId, err := parseImportID(d.Id())
 				if err != nil {
 					return nil, err
 				}
