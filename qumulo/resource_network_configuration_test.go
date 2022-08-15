c548cb291454d22fd4212bd27ba01967f3a215f5
Prajwala TM <prajwala.tm@gmail.com>
2022-08-15 11:55:59 -0700

    Add network config test

---
+++ qumulo/resource_network_configuration_test.go
@@ -0,0 +1,83 @@
+package qumulo
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"reflect"
+	"testing"
+)
+
+func TestAccNetworkConfiguration(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				ImportStateId:    "1:1",
+				ResourceName:     "qumulo_network_configuration.network_config",
+				ImportState:      true,
+				Config:           testAccNetworkConfiguration(defaultNetworkConfigurationReq),
+				ImportStateCheck: testAccCheckNetworkConfiguration(defaultNetworkConfigurationResp),
+			},
+		},
+	})
+}
+
+var defaultNetworkConfigurationReq = NetworkConfigurationRequest{
+	Name:             "Default",
+	AssignedBy:       "DHCP",
+	FloatingIpRanges: []string{},
+	DnsServers:       []string{},
+	DnsSearchDomains: []string{},
+	IpRanges:         []string{},
+	Netmask:          "",
+	Mtu:              1500,
+	VlanId:           0,
+	InterfaceId:      "1",
+	NetworkId:        "1",
+}
+
+var defaultNetworkConfigurationResp = NetworkConfigurationResponse{
+	Id:               1,
+	Name:             "Default",
+	AssignedBy:       "DHCP",
+	FloatingIpRanges: []string{},
+	DnsServers:       []string{},
+	DnsSearchDomains: []string{},
+	IpRanges:         []string{},
+	Netmask:          "",
+	Mtu:              1500,
+	VlanId:           0,
+}
+
+func testAccNetworkConfiguration(nc NetworkConfigurationRequest) string {
+	return fmt.Sprintf(`
+resource "qumulo_network_configuration" "network_config" {
+}`)
+}
+
+func testAccCheckNetworkConfiguration(nc NetworkConfigurationResponse) resource.ImportStateCheckFunc {
+	return func(s []*terraform.InstanceState) error {
+		c := testAccProvider.Meta().(*Client)
+		ctx := context.Background()
+
+		if len(s) != 1 {
+			return fmt.Errorf("expected 1 state: %+v", s)
+		}
+		res := s[0]
+		interfaceId := res.Attributes["interface_id"]
+		networkId := res.ID
+		networkConfigUri := InterfaceConfigurationEndpoint + interfaceId + NetworksEndpointSuffix + networkId
+
+		networkConfig, err := DoRequest[NetworkConfigurationRequest, NetworkConfigurationResponse](ctx, c, GET, networkConfigUri, nil)
+		if err != nil {
+			return err
+		}
+		if !reflect.DeepEqual(*networkConfig, nc) {
+			return fmt.Errorf("network config mismatch: Expected %v, got %v", nc, *networkConfig)
+		}
+		return nil
+	}
+}
